575f6daf46a47723d211e446e7cd3917ddece529
Jas0nT <taoeta@gmail.com>
2024-09-09 11:48:48 +0800

    home-manager

--- home/default.nix
+++ home/default.nix
@@ -22,17 +22,9 @@
     ./modules/cava
     ./packages
   ];
-
+  
   home = {
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "23.11";
+    stateVersion = "24.05";
 
     username = "jason";
     homeDirectory = "/home/jason";
@@ -47,4 +39,9 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  home.packages = with pkgs; [
+    home-manager
+  ];
+
 }
