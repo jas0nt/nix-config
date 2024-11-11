39c2d04083097de9ac2d5eb664e1357f359ea618
Jas0nT <taoeta@gmail.com>
2024-11-11 20:07:54 +0800

    kitty

--- home/modules/kitty/default.nix
+++ home/modules/kitty/default.nix
@@ -7,8 +7,26 @@
       size = 15.0;
     };
     settings = {
+      dynamic_background_opacity = "no";
+      background_opacity = "0.3";
+      background_blur = "0";
+
+      cursor_shape = "block";
+      cursor_blink_interval = "0";
+
+      allow_hyperlinks = "no";
       confirm_os_window_close = 0;
+      copy_on_select = "clipboard";
+      detect_urls = "no";
+      focus_follows_mouse = "yes";
+      select_by_word_characters = "$}{/&@_.";
+      strip_trailing_spaces = "smart";
     };
+    extraConfig = 
+    ''
+    mouse_map right press grabbed,ungrabbed no-op
+    mouse_map right click grabbed,ungrabbed paste_from_clipboard
+    '';
     keybindings = {
       "kitty_mod+c" = "copy_to_clipboard";
       "kitty_mod+v" = "paste_from_clipboard";
