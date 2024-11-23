92897f5f1966872a7c267a618dcf1ad3a1c06d6f
Jas0nT <taoeta@gmail.com>
2024-11-23 21:58:15 +0800

    1

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -6,8 +6,6 @@
   users.users.${const.username}.extraGroups = [ "libvirtd" ];
 
   environment.systemPackages = with pkgs; [
-    virt-manager
-    virt-viewer
     spice
     spice-gtk
     spice-protocol
