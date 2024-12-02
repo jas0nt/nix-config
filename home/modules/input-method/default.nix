915732ce4c2c5329e1e8e199bd6139b5ec255dea
Jas0nT <taoeta@gmail.com>
2024-12-02 16:27:18 +0800

    overwrite home file

--- home/modules/input-method/default.nix
+++ home/modules/input-method/default.nix
@@ -11,7 +11,7 @@
   };
 
   home.file = {
-    ".config/fcitx5/profile".source                                        = ./fcitx5/conf/profile;
+    ".config/fcitx5/profile"                                               = { source = ./fcitx5/conf/profile; target = "overwrite"; };
     ".config/fcitx5/conf/classicui.conf".source                            = ./fcitx5/conf/classicui.conf;
     ".local/share/fcitx5/themes/dracula"                                   = { recursive = true; source = ./fcitx5/themes/dracula; };
     ".local/share/fcitx5/rime/default.custom.yaml".source                  = ./fcitx5/rime/default.custom.yaml;
