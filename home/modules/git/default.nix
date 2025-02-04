6858979d3297c9b21b5e0520ef869b1deb7abc1e
Jas0nT <taoeta@gmail.com>
2025-02-04 21:23:22 +0800

    feat: add tig configuration and enable lazygit with custom theme settings

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -11,8 +11,33 @@
     ];
   };
 
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.theme = {
+        # lightTheme = true;
+        activeBorderColor = [ "#FF79C6" "bold" ];
+        inactiveBorderColor = [ "#BD93F9" ];
+        searchingActiveBorderColor = [ "#8BE9FD" "bold" ];
+        optionsTextColor = [ "#6272A4" ];
+        selectedLineBgColor = [ "#6272A4" ];
+        inactiveViewSelectedLineBgColor = [ "bold" ];
+        cherryPickedCommitFgColor = [ "#6272A4" ];
+        cherryPickedCommitBgColor = [ "#8BE9FD" ];
+        markedBaseCommitFgColor = [ "#8BE9FD" ];
+        markedBaseCommitBgColor = [ "#F1FA8C" ];
+        unstagedChangesColor = [ "#FF5555" ];
+        defaultFgColor = [ "#F8F8F2" ];
+      };
+    };
+  };
+
   home.packages = with pkgs; [
-    lazygit
     tig
   ];
+
+  home.file = {
+    ".config/tig/config" = { source = ./config/tigrc; };
+  };
+
 }
