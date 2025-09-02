113766a178c9d4ff8510eabb2d77d069ed1bc026
Jas0nT <taoeta@gmail.com>
2025-09-02 19:45:17 +0800

    rime emoji

--- home/modules/input-method/default.nix
+++ home/modules/input-method/default.nix
@@ -4,10 +4,12 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-rime
       fcitx5-configtool
       fcitx5-chinese-addons
+      rime-zhwiki
     ];
   };
 
@@ -15,7 +17,6 @@
     ".config/fcitx5/profile"                                               = { source = ./fcitx5/conf/profile; target = "overwrite"; };
     ".config/fcitx5/conf/classicui.conf".source                            = ./fcitx5/conf/classicui.conf;
     ".local/share/fcitx5/themes/dracula"                                   = { recursive = true; source = ./fcitx5/themes/dracula; };
-    ".local/share/fcitx5/rime/default.custom.yaml".source                  = ./fcitx5/rime/default.custom.yaml;
-    ".local/share/fcitx5/rime/double_pinyin_flypy.schema.yaml".source      = ./fcitx5/rime/double_pinyin_flypy.schema.yaml;
+    ".local/share/fcitx5/rime"                                             = { source = ./fcitx5/rime; recursive = true; };
   };
 }
