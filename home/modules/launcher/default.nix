e1ea745e182ce754b266f1c664cf684ad5e31981
Jas0nT <taoeta@gmail.com>
2025-09-14 09:03:13 +0800

    feat: migrate fuzzel configuration to default.nix and remove old config files

--- home/modules/launcher/default.nix
+++ home/modules/launcher/default.nix
@@ -1,18 +1,40 @@
 { pkgs, inputs, ... }:
 
 {
-  # programs.rofi.enable = true;
-
-  home.file = {
-    ".config/fuzzel/fuzzel.ini" = {
-      recursive = true;
-      source = ./config/fuzzel.ini;
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        terminal = "${pkgs.kitty}/bin/kitty";
+        layer = "overlay";
+        font = "FiraCode Nerd Font:size=16";
+        lines = 15;
+        width = 50;
+        horizontal-pad = 40;
+        vertical-pad = 10;
+        inner-pad = 10;
+        image-size-ratio = 0.8;
+        icon-theme = "BeautyLine";
+        exit-on-keyboard-focus-loss = "yes";
+      };
+      colors = {
+        background = "282a36dd";
+        text = "f8f8f2ff";
+        match = "8be9fdff";
+        selection-match = "8be9fdff";
+        selection = "44475add";
+        selection-text = "f8f8f2ff";
+        border = "bd93f9ff";
+      };
+      border = {
+        width = 2;
+        radius = 15;
+      };
     };
   };
 
   home.packages = with pkgs; [
     inputs.rauncher.defaultPackage."${pkgs.system}"
-    fuzzel
   ];
 
 }
