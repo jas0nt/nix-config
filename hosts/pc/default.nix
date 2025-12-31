80f5ea4f5a5f67cc075618736ecc659d2d826fd4
Jas0nT <taoeta@gmail.com>
2025-12-31 18:21:39 +0800

    use nixos-hardware

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -1,8 +1,22 @@
+{ config, pkgs, inputs, ... }:
+
 {
   networking.hostName = "pc";
   imports =
     [
       ./hardware-configuration.nix
-      ../hardware/nvidia-config.nix
+      # ../hardware/nvidia-config.nix
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+      inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
     ];
+
+
+  hardware.nvidia = {
+    open = false; 
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.nvidia-container-toolkit.enable = true;
 }
