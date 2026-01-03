e18ac266bac6117c482263247f26fc4869f637c4
Jas0nT <taoeta@gmail.com>
2026-01-03 11:48:39 +0800

    fix steam audio

--- system/game.nix
+++ system/game.nix
@@ -1,9 +1,17 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, ... }:
 
 {
-  programs = {
-    steam = {
-      enable = true;
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      extraProfile = ''
+      ## PULSE_LATENCY_MSEC=60 %command% -sdlaudiodriver pulse -perfectworld
+      # 1. Force PulseAudio driver (equivalent to -sdlaudiodriver pulse)
+      export SDL_AUDIODRIVER=pulse
+      
+      # 2. Increase Audio Latency (equivalent to PULSE_LATENCY_MSEC=60)
+      export PULSE_LATENCY_MSEC=60
+    '';
     };
   };
 
