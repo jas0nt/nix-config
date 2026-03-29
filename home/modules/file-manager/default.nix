63726d506889d9f10a0de63b9430af61b62a3bc6
Jas0nT <taoeta@gmail.com>
2026-03-29 09:07:23 +0800

    bind q to escape

--- home/modules/file-manager/default.nix
+++ home/modules/file-manager/default.nix
@@ -82,6 +82,7 @@
         { run = "shell --orphan --confirm ${const.terminal}"; on = [ "T" ]; }
         { run = "shell 't=$(mktemp --suffix=.jpg); ffmpegthumbnailer -i \"$0\" -o \"$t\" -s 0 -c jpeg -f >/dev/null 2>&1; qview \"$t\" >/dev/null 2>&1; rm \"$t\"' --block"; on = [ "e" ]; }
 
+        { run = "escape";                         on = [ "q" ]; }
         { run = "remove";                         on = [ "D" ]; }
         { run = "remove --permanently";           on = [ "d" ]; }
         { run = "shell 'dua i .' --block";        on = [ "m" "S" ]; }
