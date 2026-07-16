fef86cc78dd46ae99d6b16cd01c7adcb3f34e800
Jas0nT <taoeta@gmail.com>
2026-07-17 07:53:38 +0800

    mpv

--- home/modules/player/default.nix
+++ home/modules/player/default.nix
@@ -21,11 +21,23 @@
         profile = "gpu-hq";
         slang = "zh-Hans,zh-CN,zh-cn,sc,Chinese (Simplified),chi,zh,zho,tc,en,eng";
         sub-auto = "all";
+
+        gpu-context = "wayland";
+        vo = "gpu-next";
+
+        save-position-on-quit = "yes";
+        watch-later-directory = "~~/watch_later";
+        directory-mode = "playlist";
       };
       scripts = with pkgs.mpvScripts; [
         mpris
         webtorrent-mpv-hook
       ];
     };
+  };
+
+  xdg.configFile."mpv/scripts/autoload.lua".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/lua/autoload.lua";
+    hash = "sha256-wO9awsko9qDTRkwUpAMQSWjXM5yx7XzaZClF+EINh0Q=";
   };
 }
